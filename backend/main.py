bd6e704e17bc79b1e1a60ca148108babd5b823cc
KlementevYP <ypklementev@icloud.com>
2024-12-06 17:02:42 +0300

    Отключение ненужных элементов, настройка работы почты

--- backend/main.py
+++ backend/main.py
@@ -1,10 +1,13 @@
 from fastapi import FastAPI, Request, Form
 from fastapi.templating import Jinja2Templates
 from fastapi.staticfiles import StaticFiles
-from fastapi.responses import HTMLResponse
+from fastapi.responses import HTMLResponse, RedirectResponse
 from sqlalchemy.orm import Session
 from database import engine, Blog, Person, About, Resume, Education, Experience, Skill, PortfolioProjects
 import bcrypt
+import smtplib
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
 from datetime import datetime, date
 from babel.dates import format_date
 
@@ -53,6 +56,36 @@ async def read_index(request: Request):
 
     return templates.TemplateResponse("index.html", context)
 
+@app.post("/contact")
+async def handle_contact_form(
+    fullname: str = Form(...),
+    email: str = Form(...),
+    message: str = Form(...)
+):
+    # Настройки SMTP
+    smtp_server = "smtp.gmail.com"
+    smtp_port = 587
+    smtp_user = "klementev712@gmail.com"
+    smtp_password = "noxj kwcw jeby lxnc"
+
+    # Создание сообщения
+    msg = MIMEMultipart()
+    msg["From"] = smtp_user
+    msg["To"] = "klementev712@gmail.com"  # Ваш email
+    msg["Subject"] = "Новое сообщение с сайта-портфолио"
+
+    body = f"Имя: {fullname}\nEmail: {email}\nСообщение:\n{message}"
+    msg.attach(MIMEText(body, "plain"))
+
+    # Отправка письма
+    try:
+        with smtplib.SMTP(smtp_server, smtp_port) as server:
+            server.starttls()
+            server.login(smtp_user, smtp_password)
+            server.send_message(msg)
+        return RedirectResponse(url="/", status_code=303)
+    except Exception as e:
+        return RedirectResponse(url="/", status_code=303)
 
 @app.get('/edit')
 async def edit_page(request: Request):
@@ -100,4 +133,4 @@ async def edit_page(request: Request):
 
 if __name__ == '__main__':
     import uvicorn
-    uvicorn.run(app, host='0.0.0.0', port=8000)
+    uvicorn.run(app, host='192.168.0.200', port=8000)
