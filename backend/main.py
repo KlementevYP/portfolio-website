e508e0146e048ceb4b41a956b09c5d94e73f7bc4
KlementevYP <ypklementev@icloud.com>
2024-12-08 15:00:00 +0300

    refactor

--- backend/main.py
+++ backend/main.py
@@ -2,8 +2,6 @@ from fastapi import FastAPI, Request, Form
 from fastapi.templating import Jinja2Templates
 from fastapi.staticfiles import StaticFiles
 from fastapi.responses import HTMLResponse, RedirectResponse
-from sqlalchemy.orm import Session
-from database import engine, Blog, Person, About, Resume, Education, Experience, Skill, PortfolioProjects
 import bcrypt
 import smtplib
 from email.mime.text import MIMEText
@@ -24,37 +22,7 @@ templates.env.filters['date'] = format_date_ru  # Регистрация фил�
 
 @app.get('/')
 async def read_index(request: Request):
-    with Session(engine) as session:
-        person = session.query(Person).first()
-        if person is None:
-            return templates.TemplateResponse("error.html", {"request": request, "message": "person not found"}, status_code=404)
-        about = session.query(About).all()
-        blogs = session.query(Blog).all()
-        resume = session.query(Resume).all()
-        education = []
-        experience = []
-        skills = []
-        for res in resume:
-            education.extend(res.educations)
-            experience.extend(res.experiences)
-            skills.extend(res.skills)
-        portfolio_projects = session.query(PortfolioProjects).all()
-        blog = session.query(Blog).all()
-
-    context = {
-        'request': request,
-        'person': person,
-        'about': about,
-        'blogs': blogs,
-        'resume': resume,
-        'education': education,
-        'experience': experience,
-        'skills': skills,
-        'portfolio_projects': portfolio_projects,
-        'blog': blog,
-    }
-
-    return templates.TemplateResponse("index.html", context)
+    return templates.TemplateResponse("index.html", context={"request": request})
 
 @app.post("/contact")
 async def handle_contact_form(
@@ -87,50 +55,6 @@ async def handle_contact_form(
     except Exception as e:
         return RedirectResponse(url="/", status_code=303)
 
-@app.get('/edit')
-async def edit_page(request: Request):
-    return templates.TemplateResponse("edit_info.html", {"request": request})
-
-# @app.get("/create-person", response_class=HTMLResponse)
-# async def create_person_form(request: Request):
-#     return templates.TemplateResponse("create_person.html", {"request": request})
-
-# @app.post("/create-person")
-# async def create_person(
-#     name: str = Form(...),
-#     age: int = Form(...),
-#     profession: str = Form(...),
-#     location: str = Form(...),
-#     email: str = Form(...),
-#     phone: str = Form(...),
-#     birth_date: str = Form(...),
-#     login: str = Form(...),
-#     password: str = Form(...),
-# ):
-#     # Хешируем пароль
-#     hashed_password = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
-
-#     # Преобразуем строку даты в объект date
-#     birth_date_obj = datetime.strptime(birth_date, "%Y-%m-%d").date()
-
-#     with Session(engine) as session:
-#         new_person = Person(
-#             name=name,
-#             age=age,
-#             profession=profession,
-#             location=location,
-#             email=email,
-#             phone=phone,
-#             birth_date=birth_date_obj,  # Используем объект date
-#             login=login,
-#             password=hashed_password.decode('utf-8')
-#         )
-#         session.add(new_person)
-#         session.commit()
-#         session.refresh(new_person)
-
-#     return {"message": "Person created successfully"}
-
 if __name__ == '__main__':
     import uvicorn
-    uvicorn.run(app, host='192.168.0.200', port=8000)
+    uvicorn.run(app, host='0.0.0.0', port=8000)
